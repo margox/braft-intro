f87ea7d471c31ae84435fb9557ba244c6d6fb61a
Margox <margox@foxmail.com>
2018-09-21 17:47:58 +0800

    N/A

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,7 +1,15 @@
+import './styles.scss'
 import React from 'react'
 
 export default () => (
   <div className="page-index">
-    <div className="header"></div>
+    <div className="header">
+      <h1 className="caption">Braft Editor</h1>
+      <h2 className="sub-caption">美观好用的React富文本编辑器</h2>
+      <div className="buttons">
+        <button className="button-view-doc">查看文档</button>
+        <button className="button-fork">Star &amp; Fork</button>
+      </div>
+    </div>
   </div>
 )
